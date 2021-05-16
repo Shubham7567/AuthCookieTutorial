0a98d6c525a01187387d2cac902a8189497f207d
shubham7567 <shubhamrathore7567@gmail.com>
2021-05-16 12:39:43 +0530

    Add project files.

---
+++ AuthCookieTutorial/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using AuthCookieTutorial.Models;
+using DotNetOpenAuth.InfoCard;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthCookieTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(String txtUserName, String txtPassword)
+        {
+            if (txtUserName.ToLower() == "admin" && txtPassword == "123")
+            {
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name,txtUserName)
+                };
+                
+                var identity = new ClaimsIdentity(
+                        claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+                var props = new AuthenticationProperties();
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,principal,props).Wait();
+
+                return RedirectToAction("Index", "Employee");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AuthCookieTutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthCookieTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
